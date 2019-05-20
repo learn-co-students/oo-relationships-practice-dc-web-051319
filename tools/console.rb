f5770fe65199d28b5238bed501f6e01dc8e12988
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-05-20 15:33:39 -0400

    initialized bakery

--- tools/console.rb
+++ tools/console.rb
@@ -3,7 +3,7 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-
+# AirBnB
 spring_break = Listing.new("Cancun")
 
 fall_break = Listing.new("DC")
@@ -16,6 +16,12 @@ trip2 = Trip.new(fall_break, molly)
 trip2 = Trip.new(fall_break, molly)
 trip2 = Trip.new(fall_break, molly)
 
+# Bakery
+bakery1 = Bakery.new()
+bakery2 = Bakery.new()
+bakery3 = Bakery.new()
+bakery4 = Bakery.new()
 
+dessert1 = Bakery.new()
 
 Pry.start
